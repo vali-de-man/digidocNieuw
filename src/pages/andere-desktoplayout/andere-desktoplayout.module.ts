c5d1e1701040eed9b97ccab4393a4d6c9dd2de82
Valentijn <vali-de-man@hotmail.com>
2017-09-21 12:36:50 +0200

    Nu wel zeven nieuwe files

---
+++ src/pages/andere-desktoplayout/andere-desktoplayout.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { AndereDesktoplayoutPage } from './andere-desktoplayout';
+
+@NgModule({
+  declarations: [
+    AndereDesktoplayoutPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(AndereDesktoplayoutPage),
+  ],
+})
+export class AndereDesktoplayoutPageModule {}
