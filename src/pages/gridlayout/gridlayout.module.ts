d346ff65a261e0a0400b14e3231dae41cb316b2c
pniemantsverdriet <pniemantsverdriet@me.com>
2017-09-22 11:53:48 +0200

    merge vr 22/9/17 12:00

---
+++ src/pages/gridlayout/gridlayout.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { GridlayoutPage } from './gridlayout';
+
+@NgModule({
+  declarations: [
+    GridlayoutPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(GridlayoutPage),
+  ],
+})
+export class GridlayoutPageModule {}
